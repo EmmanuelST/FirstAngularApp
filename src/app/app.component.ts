6235907f2bcda539f1a7ba06c78dd4be7bfe903f
EmmanuelST <manuel.413@hotmail.com>
2024-05-15 23:43:19 -0400

    configure project routes in app component and app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ClientesComponent } from './clientes/clientes.component';
 import { NavbarComponent } from "./navbar/navbar.component";
-import { DireccionComponent } from "./direccion/direccion.component";
+import { DireccionComponent } from "./modules/direccion/direccion/direccion.component";
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { DireccionModule } from './modules/direccion/direccion.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ClientesComponent, NavbarComponent, DireccionComponent]
+    imports: [RouterOutlet,ClientesModule, NavbarComponent, DireccionModule]
 })
 export class AppComponent {
   title = 'my-app';
