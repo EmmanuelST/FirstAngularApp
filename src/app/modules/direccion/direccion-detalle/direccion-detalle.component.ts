84b45270865c3e3219d11e3df74a44d7dc3eacb1
EmmanuelST <manuel.413@hotmail.com>
2024-05-22 22:30:30 -0400

    update direccion UIs to use Observers and new service methods

--- src/app/modules/direccion/direccion-detalle/direccion-detalle.component.ts
+++ src/app/modules/direccion/direccion-detalle/direccion-detalle.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
-import { CardContainerComponent } from "../../../card-container/card-container.component";
 import { DireccionService } from '../services/direccion.service';
 import { ActivatedRoute } from '@angular/router';
 import { Direccion } from '../../../models/direccion';
+import { Observable, of } from 'rxjs';
 
 @Component({
-    selector: 'app-direccion-detalle',
-    standalone: false,
-    templateUrl: './direccion-detalle.component.html',
-    styleUrl: './direccion-detalle.component.css'
+  selector: 'app-direccion-detalle',
+  standalone: false,
+  templateUrl: './direccion-detalle.component.html',
+  styleUrl: './direccion-detalle.component.css'
 })
 export class DireccionDetalleComponent {
-  constructor(direccionService:DireccionService, routeManager: ActivatedRoute){
+
+  direccion: Observable<Direccion | undefined> = of(undefined);
+
+  constructor(direccionService: DireccionService, routeManager: ActivatedRoute) {
     routeManager.params.subscribe(params => {
-      if(params["id"]){
-        this.direccion = direccionService.getDireccion(params["id"])
+      if (params["id"]) {
+        this.direccion = direccionService.getDireccionById(params["id"])
       }
     })
-      
   }
-
-  direccion:Direccion = new Direccion();
 }
